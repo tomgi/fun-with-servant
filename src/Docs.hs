c2fb793850ca8954532bc21c2aa485d6302e9d1c
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2018-02-09 15:35:09 +0100

    Added autogenerated docs

---
+++ src/Docs.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings     #-}
+
+module Docs (server) where
+
+import           Data.Default            (def)
+import           Data.Text.Lazy          (pack)
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+import           Network.HTTP.Types
+import           Network.Wai
+import           Servant.API
+import           Servant.Docs
+import           Servant.Docs.Pandoc
+import           Servant.Server
+import qualified Server
+import           Text.Pandoc
+import           Types
+
+instance ToSample User where
+  toSamples _ = singleSample $ User 1 "Isaac" "Newton"
+
+instance ToSample DateTimeAndIP where
+  toSamples _ = singleSample $ DateTimeAndIP (DateTime "02:42:35 PM" "02-09-2018" 1518187355975) (IP "127.0.0.1")
+
+apiDocs :: API
+apiDocs = docs Server.api
+
+server :: Server Raw
+server = Tagged serveDocs where
+    serveDocs _ respond =
+        respond $ responseLBS ok200 [html] $ (encodeUtf8 . pack) $ writeHtmlString def (pandoc apiDocs)
+    html = ("Content-Type", "text/html")
--- src/Server.hs
+++ src/Server.hs
@@ -1,30 +1,16 @@
 {-# LANGUAGE DataKinds     #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Server (startApp) where
+module Server (API, api, server) where
 
 import qualified Client
 import           Control.Monad.IO.Class
 import           Data.ByteString.Lazy.Char8 (pack)
-import           Data.Maybe
-import           Network.Wai.Handler.Warp
 import           Servant
-import           System.Environment
-import           Text.Read
 import           Types
 
 type API = "users" :> Get '[JSON] [User]
       :<|> "time"  :> Get '[JSON] DateTimeAndIP
-
-startApp :: IO ()
-startApp = do
-  port <- lookupEnv "PORT"
-  let portNumber = fromMaybe 8080 (port >>= readMaybe)
-  putStrLn $ "Listening on " ++ show portNumber
-  run portNumber app
-
-app :: Application
-app = serve api server
 
 api :: Proxy API
 api = Proxy
